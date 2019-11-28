ef60310b7726c9eced0c815ffdf318601768086f
Remi Maeda <berytopuer@i.softbank.jp>
2019-11-28 06:52:57 +0000

    課題４提出

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /[^\s]@[^\s]\.[^\s]/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\Z/i }
+  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\Z/i }
+  has_secure_password
+  
+end
