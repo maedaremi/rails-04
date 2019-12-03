eaad7cd02c8e11c8744fe32782dc82d8fa0630ef
Remi Maeda <berytopuer@i.softbank.jp>
2019-12-03 14:52:39 +0000

    課題６提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: { with: /[^\s]@[^\s]\.[^\s]/ }
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\Z/i }
-  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\Z/i }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\Z/i }
+  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\Z/i }
   has_secure_password
+  
+  has_many :topics
   
 end
